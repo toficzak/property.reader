7fed4f6d1dc0112ec9bad9e5fe5d4298ba49edf2
Krzysztof Kucharczyk <krzysztof.kucharczyk@fream.pl>
2020-11-28 12:24:03 +0100

    pom update.

--- src/main/java/krzysztof/property/reader/PropertyReader.java
+++ src/main/java/krzysztof/property/reader/PropertyReader.java
@@ -7,50 +7,67 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * TODO: potencjalnie fajnie by było tworzyć w projektach dziedziczących enum'a
- * z możliwymi wartościami, przyjmować go w konstruktorze i pozwalać wybierać
- * tylko wśród nich
- **/
+ *
+ * <p>
+ * How to use this class:<br />
+ * <br />
+ * <em>private final PropertyReader propertyReader;</em><br />
+ * <em>private final String dbHost;</em>
+ * </p>
+ * <p>
+ * <em>public DatabaseConnector() {</em><br />
+ * <em>this.propertyReader = new PropertyReader("config.properties",
+ * this.getClass().getClassLoader());</em>
+ * </p>
+ * <p>
+ * <em>this.dbHost = propertyReader.getProperty("db.host");</em><br />
+ * <em>}</em>
+ * </p>
+ * 
+ * @author krzysztof
+ *
+ */
 public class PropertyReader {
 
-	private static final Logger LOGGER = Logger.getLogger(PropertyReader.class.getSimpleName());
+  private static final Logger LOGGER = Logger.getLogger(PropertyReader.class.getSimpleName());
 
-	private final String propertyFilePath;
-	private final Properties properties;
-	private final ClassLoader cl;
+  private final String propertyFilePath;
+  private final Properties properties;
+  private final ClassLoader cl;
 
-	public PropertyReader(String propertyFilePath, ClassLoader cl) {
-		this.propertyFilePath = propertyFilePath;
-		this.cl = cl;
-		this.properties = this.loadPropValues();
-	}
+  public PropertyReader(String propertyFilePath, ClassLoader cl) {
+    this.propertyFilePath = propertyFilePath;
+    this.cl = cl;
+    this.properties = this.loadPropValues();
+  }
 
-	public String getProperty(String key) {
-		Object prop = properties.get(key);
-		if (prop == null) {
-			String message = String.format("No property: %s.", key);
-			LOGGER.log(Level.WARNING, message);
-			throw new PropertyLoadException(message);
-		}
-		return (String) prop;
-	}
+  public String getProperty(String key) {
+    Object prop = properties.get(key);
+    if (prop == null) {
+      String message = String.format("No property: %s.", key);
+      LOGGER.log(Level.WARNING, message);
+      throw new PropertyLoadException(message);
+    }
+    return (String) prop;
+  }
 
-	private Properties loadPropValues() {
+  private Properties loadPropValues() {
 
-		if (this.propertyFilePath == null || this.propertyFilePath.isBlank()
-				|| Objects.isNull(cl.getResource(propertyFilePath))) {
-			String message = String.format("property file '%s' not found in the classpath", propertyFilePath);
-			LOGGER.log(Level.WARNING, message);
-			throw new PropertyLoadException("No config file.");
-		}
+    if (this.propertyFilePath == null || this.propertyFilePath.isBlank()
+        || Objects.isNull(cl.getResource(propertyFilePath))) {
+      String message =
+          String.format("property file '%s' not found in the classpath", propertyFilePath);
+      LOGGER.log(Level.WARNING, message);
+      throw new PropertyLoadException("No config file.");
+    }
 
-		try (InputStream inputStream = cl.getResourceAsStream(propertyFilePath)) {
-			Properties props = new Properties();
-			props.load(inputStream);
-			return props;
-		} catch (Exception e) {
-			LOGGER.log(Level.WARNING, String.format("Exception: %s", e));
-			throw new PropertyLoadException();
-		}
-	}
+    try (InputStream inputStream = cl.getResourceAsStream(propertyFilePath)) {
+      Properties props = new Properties();
+      props.load(inputStream);
+      return props;
+    } catch (Exception e) {
+      LOGGER.log(Level.WARNING, String.format("Exception: %s", e));
+      throw new PropertyLoadException();
+    }
+  }
 }
